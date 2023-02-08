f43fb7419220ebbcb851b19ea25ec2376aaaa0d6
Marcin Gwizdek <programowanie.kodilla@gmail.com>
2023-02-08 16:21:52 +0100

    add separate app into components

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,26 +1,10 @@
 import styles from './Product.module.scss';
-import clsx from 'clsx';
-import Button from '../Button/Button';
 import { PropTypes } from 'prop-types';
 import { useState } from 'react';
-import shortid from 'shortid';
+import ProductImage from '../ProductImage/ProductImage.js';
+import ProductForm from '../ProductForm/ProductForm.js';
 
 const Product = (props) => {
-  Product.propTypes = {
-    id: PropTypes.number,
-    name: PropTypes.string,
-    title: PropTypes.string,
-    basePrice: PropTypes.number,
-    colors: PropTypes.array,
-    sizes: PropTypes.array,
-  };
-
-  const prepareColorClassName = (color) => {
-    return styles[
-      'color' + color[0].toUpperCase() + color.substr(1).toLowerCase()
-    ];
-  };
-
   const getPrice = () => {
     return props.basePrice + currentPrice;
   };
@@ -31,65 +15,47 @@ const Product = (props) => {
     props.sizes[0].additionalPrice
   );
 
+  const cartSummary = {
+    name: props.title,
+    price: getPrice(),
+    size: currentSize,
+    color: currentColor,
+  };
+
   return (
     <article className={styles.product}>
-      <div className={styles.imageContainer}>
-        <img
-          className={styles.image}
-          alt={props.title + 'shirt'}
-          src={`${process.env.PUBLIC_URL}/images/products/shirt-${props.name}--${currentColor}.jpg`}
-        />
-      </div>
+      <ProductImage
+        title={props.title}
+        name={props.name}
+        color={currentColor}
+      />
       <div>
         <header>
           <h2 className={styles.name}>{props.title}</h2>
           <span className={styles.price}>Price: {getPrice()} $</span>
         </header>
-        <form>
-          <div className={styles.sizes}>
-            <h3 className={styles.optionLabel}>Sizes</h3>
-            <ul className={styles.choices}>
-              {props.sizes.map((size) => (
-                <li key={shortid()}>
-                  <button
-                    onClick={() => {
-                      setCurrentSize(size.name);
-                      setCurrentPrice(size.additionalPrice);
-                    }}
-                    className={clsx(size.name === currentSize && styles.active)}
-                  >
-                    {size.name}
-                  </button>
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div className={styles.colors}>
-            <h3 className={styles.optionLabel}>Colors</h3>
-            <ul className={styles.choices}>
-              {props.colors.map((color) => (
-                <li key={color}>
-                  <button
-                    onClick={() => {
-                      setCurrentColor(color);
-                    }}
-                    type='button'
-                    className={clsx(
-                      prepareColorClassName(color),
-                      color === currentColor && styles.active
-                    )}
-                  ></button>
-                </li>
-              ))}
-            </ul>
-          </div>
-          <Button className={styles.button}>
-            <span className='fa fa-shopping-cart' />
-          </Button>
-        </form>
+        <ProductForm
+          sizes={props.sizes}
+          //size={props.size}
+          setCurrentSize={setCurrentSize}
+          setCurrentPrice={setCurrentPrice}
+          currentSize={currentSize}
+          colors={props.colors}
+          setCurrentColor={setCurrentColor}
+          cartSummary={cartSummary}
+          currentColor={currentColor}
+        />
       </div>
     </article>
   );
+};
+Product.propTypes = {
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  basePrice: PropTypes.number.isRequired,
+  colors: PropTypes.array.isRequired,
+  sizes: PropTypes.array.isRequired,
 };
 
 export default Product;
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,22 +1,22 @@
 import { useState } from 'react';
 import productsData from '../../data/products';
 import Product from '../Product/Product';
+import shortid from 'shortid';
 
 const Products = () => {
   const [products] = useState(productsData);
   return (
     <section>
       {products.map((product) => (
-        <Product
-          key={product.id}
-          id={products.id}
-          name={product.name}
-          title={product.title}
-          colors={product.colors}
-          sizes={product.sizes}
-          basePrice={product.basePrice}
-          //<Product {[...product[product]} />
-        />
+        <Product key={shortid()} {...product} />
+        // key={shortid()}
+        // id={products.id}
+        // name={product.name}
+        // title={product.title}
+        // colors={product.colors}
+        // sizes={product.sizes}
+        // basePrice={product.basePrice}
+        // />
       ))}
     </section>
   );
